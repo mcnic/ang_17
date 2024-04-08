223c4650064d43e7fa00f07ae1170919cdbdd42a
max <mcnic@users.noreply.github.com>
2024-04-08 16:15:56 -0700

    feat: add 2 test components

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HousingLocationComponent } from '../housingLocation/housingLocation.component';
+import { Housinglocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [HousingLocationComponent],
+})
+export class HomeComponent implements OnInit {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/housingLocation/housingLocation.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  templateUrl: './housingLocation.component.html',
+  styleUrls: ['./housingLocation.component.css'],
+  standalone: true,
+})
+export class HousingLocationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
