74c93e2c9ee2824826c0c22e251b895b1545c816
max <mcnic@users.noreply.github.com>
2024-04-08 22:14:10 -0700

    feat: add form

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,12 +2,15 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housingLocation';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class DetailsComponent implements OnInit {
   route: ActivatedRoute = inject(ActivatedRoute);
@@ -15,10 +18,24 @@ export class DetailsComponent implements OnInit {
   housingLocationId = -1;
   housingLocation: HousingLocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor() {
     this.housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(
       this.housingLocationId
+    );
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
     );
   }
 
